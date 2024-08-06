8c7634ecd6f7866a3bc14e5584b29db339a1e473
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-08-05 22:45:57 -0300

    removido codigo nao usado e refatorado msg a ser exibida

--- internal/infra/webserver/handlers/cep_handler.go
+++ internal/infra/webserver/handlers/cep_handler.go
@@ -10,29 +10,6 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type ViaCep struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Unidade     string `json:"unidade"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
-}
-
-type BrasilApi struct {
-	Cep          string `json:"cep"`
-	State        string `json:"state"`
-	City         string `json:"city"`
-	Neighborhood string `json:"neighborhood"`
-	Street       string `json:"street"`
-	Service      string `json:"service"`
-}
-
 func SearchCepHandler(w http.ResponseWriter, r *http.Request) {
 	cep := chi.URLParam(r, "cep")
 	if cep == "" {
@@ -68,5 +45,5 @@ func makeRequest(apiName string, apiUrl string, resultChannel chan string) {
 		log.Fatal(err)
 	}
 
-	resultChannel <- fmt.Sprintf("Resultado da %s: %s", apiName, string(body))
+	resultChannel <- fmt.Sprintf("API vencedora: %s\nDados: %s", apiName, string(body))
 }
