b0656884faf759fb34c91f0b53550ebdd0e1bd16
Pedro Gutierres <78659993+pedrogutierresbr@users.noreply.github.com>
2024-08-05 22:39:16 -0300

    criado routines, chanels, make request e feito select para obter resultado

--- internal/infra/webserver/handlers/cep_handler.go
+++ internal/infra/webserver/handlers/cep_handler.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -36,5 +40,33 @@ func SearchCepHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("CEP: " + cep))
+	viaCepChannel := make(chan string)
+	brasilApiChannel := make(chan string)
+
+	go makeRequest("ViaCEP", "https://viacep.com.br/ws/"+cep+"/json/", viaCepChannel)
+	go makeRequest("BrasilAPI", "https://brasilapi.com.br/api/cep/v1/"+cep, brasilApiChannel)
+
+	select {
+	case msg := <-viaCepChannel:
+		fmt.Println(msg)
+	case msg := <-brasilApiChannel:
+		fmt.Println(msg)
+	case <-time.After(1 * time.Second):
+		fmt.Println("Erro de timeout")
+	}
+}
+
+func makeRequest(apiName string, apiUrl string, resultChannel chan string) {
+	resp, err := http.Get(apiUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resultChannel <- fmt.Sprintf("Resultado da %s: %s", apiName, string(body))
 }
